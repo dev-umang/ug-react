9aaff69d9b7a8e28ddef2aeb40993be283b18ed4
Umang Joshi <umang@protocolzone.com>
2024-01-14 13:49:26 +0530

    Testing eslint commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+// import {useState} from "react";
+
+// import viteLogo from "/vite.svg";
+
+// import reactLogo from "./assets/react.svg";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      HELLO
+    </>
+  );
+}
+
+export default App;
